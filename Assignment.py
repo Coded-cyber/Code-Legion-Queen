f377008cfd7ca5fff4d8c1c2ec9869f9d4f329d8
Coded-cyber <dreamgirllaurel18@gmail.com>
2026-04-25 13:57:23 +1300

    Create Assignment.py

---
+++ Assignment.py
@@ -0,0 +1,12 @@
+first_name = input("What is your first name? ")
+last_name = input("What is your last name? ")
+age = int(input("How old are you? "))
+favorite_color = input("What is your favorite color? ")
+hometown = input("What is your hometown? ")
+
+print("\n--- Your Introduction Card ---")
+print()
+print(f"Name: {first_name} {last_name}")
+print(f"Age: {age}")
+print(f"Favorite color: {favorite_color}")
+print(f"Hometown: {hometown}")
